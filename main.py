7e5f3c83cac26bf4a6c94c5a48b248d3839d04d0
Artem Pashkov <a.pashkov@bk.ru>
2020-10-18 14:50:44 +0300

    Create project

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+def start_project(string):
+    print(string)
+
+
+if __name__ == '__main__':
+    start_project('Hello World')
